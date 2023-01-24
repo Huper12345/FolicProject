2384bd8e5f456f93060dee3902092a8184fcd9b9
Huper12345 <115031997+Huper12345@users.noreply.github.com>
2023-01-24 15:47:14 +0300

    update pictures and media

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,7 +1,7 @@
-import React from "react"
-import "./Intro.scss";
-import Girl from '../../helpers/assets/Images/dev2.webp'
-import { TextBlock } from "./TextBlock";
+import React from 'react';
+import './Intro.scss';
+import Girl from '../../helpers/assets/Images/dev2.webp';
+import { TextBlock } from './TextBlock';
 
 export const Intro = () => {
   return (
@@ -9,7 +9,12 @@ export const Intro = () => {
       <div className="container">
         <div className="Intro-Inner">
           <TextBlock />
-          <img className="Intro-Background" alt="Girl" src="https://i.ibb.co/wBY3zYq/dev2.webp"></img>
+          <img
+            className="Intro-Background"
+            alt="Girl"
+            src="https://i.ibb.co/XXqtmxW/devushka5.png"
+            alt="devushka3"
+          ></img>
         </div>
       </div>
     </div>
