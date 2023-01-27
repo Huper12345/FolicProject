2a3a73265ee7f8c9820554101262513d871ce585
Huper12345 <115031997+Huper12345@users.noreply.github.com>
2023-01-27 14:17:55 +0300

    Add block IntFacts

---
+++ src/components/IntFacts/IntFacts.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./IntFacts.css";
+
+export const IntFacts = () => {
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="IntFacts">
+         
+        </div>
+      </div>
+    </section>
+  );
+};
