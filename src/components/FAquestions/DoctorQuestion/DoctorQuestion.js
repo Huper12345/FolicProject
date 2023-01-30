5fcf85cd0b7f43ee58e496e7aa3e2ebf0527e87b
Huper12345 <115031997+Huper12345@users.noreply.github.com>
2023-01-30 13:30:46 +0300

    update mail sistem for DoctorQuestion component

--- src/components/FAquestions/DoctorQuestion/DoctorQuestion.js
+++ src/components/FAquestions/DoctorQuestion/DoctorQuestion.js
@@ -1,14 +1,15 @@
-import React from "react"
-import "./DoctorQuestion.css";
-import { useState } from "react";
-
+import React from 'react';
+import './DoctorQuestion.css';
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 export const DoctorQuestion = () => {
-  
   const [nameText, setNameText] = useState('');
-  const [emailText, setEmailText] = useState('')
+  const [emailText, setEmailText] = useState('');
   const [textAreaText, setTextArea] = useState('');
-  
+
+  const form = useRef();
 
   const nameSelect = (e) => {
     setNameText(e.target.value);
@@ -21,39 +22,66 @@ export const DoctorQuestion = () => {
   const textAreaSelect = (e) => {
     setTextArea(e.target.value);
   };
-  
-  
-    return (
-    <div className="DoctorQuestion-Container">
-        <h2 className="Doctor-Question-Title"> Задать вопрос врачу</h2>
-        <form method="post" action="./mail.php" id="Question-Form" className="Question-Form">
-            <input 
-            type="text" 
-            placeholder="Ваше имя" 
-            className="Question-Input"
-            name="name"
-            value={nameText}
-            onChange={nameSelect}>
-            </input>
-            <input 
-            type="email" 
-            placeholder="Email-адрес" 
-            className="Question-Input"
-            name="email"
-            value={emailText}
-            onChange={emailSelect}>
-            </input>
-            <textarea 
-            placeholder="Напишите вопрос"
-            name="message" 
-            className="Question-Message"
-            value={textAreaText}
-            onChange={textAreaSelect}>
 
-            </textarea>
-        </form>
-        <button form="Question-Form" className="QuestionButton">Отправить</button>
-       
+  const sendEmail = (e) => {
+    e.preventDefault(); // prevents the page from reloading when you hit “Send”
+    emailjs
+      .sendForm(
+        'service_x6kt42c',
+        'template_q9iaa2a',
+        form.current,
+        'FKDMCs7z1Y7TOoCpl'
+      )
+      .then(
+        (result) => {
+          alert('Вопрос отправлен!');
+        },
+        (error) => {
+          alert('Что-то пошло не так...');
+        }
+      );
+
+    setNameText('');
+    setEmailText('');
+    setTextArea('');
+  };
+
+  return (
+    <div className="DoctorQuestion-Container">
+      <h2 className="Doctor-Question-Title"> Задать вопрос врачу</h2>
+      <form
+        onSubmit={sendEmail}
+        ref={form}
+        id="Question-Form"
+        className="Question-Form"
+      >
+        <input
+          type="text"
+          placeholder="Ваше имя"
+          className="Question-Input"
+          name="name"
+          value={nameText}
+          onChange={nameSelect}
+        ></input>
+        <input
+          type="email"
+          placeholder="Email-адрес"
+          className="Question-Input"
+          name="email"
+          value={emailText}
+          onChange={emailSelect}
+        ></input>
+        <textarea
+          placeholder="Напишите вопрос"
+          name="message"
+          className="Question-Message"
+          value={textAreaText}
+          onChange={textAreaSelect}
+        ></textarea>
+      </form>
+      <button form="Question-Form" className="QuestionButton">
+        Отправить
+      </button>
     </div>
   );
 };
