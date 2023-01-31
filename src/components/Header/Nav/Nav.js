06856545388eda36d4282cfa505da388ddb8044a
Huper12345 <115031997+Huper12345@users.noreply.github.com>
2023-01-31 15:00:18 +0300

    Add nav drop block

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Nav.css';
+import { NavDrop } from './NavDrop';
 
 export const Nav = () => {
   return (
     <nav className="Nav">
-      <a href="#" className="Nav-Link">
-        О препарате
-      </a>
+      <NavDrop
+        buttonText="О препарате"
+        href="#Medication"
+        droptext="Инструкция"
+        href1="https://www.rceth.by/NDfiles/instr/23_01_3103_s.pdf"
+        target1="blank"
+        droptext2="Вопрос врачу"
+        href2="#DoctorQuestion"
+        droptext3="Часто задаваемые вопросы"
+        href3="#FAquestions"
+      />
       <a href="#" className="Nav-Link">
         Задать вопрос
       </a>
---
+++ src/components/Header/Nav/NavDrop/NavDrop.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { useState } from 'react';
+import './NavDrop.css';
+
+export const NavDrop = ({
+  buttonText,
+  droptext,
+  droptext2,
+  droptext3,
+  NavClass = 'NavDrop',
+  href = '#',
+  href1 = '#',
+  href2 = '#',
+  href3 = '#',
+  target = '',
+  target1 = '',
+  target2 = '',
+  target3 = '',
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={NavClass} onMouseLeave={handleClose}>
+      <a
+        href={href}
+        target={target}
+        onMouseEnter={handleOpen}
+        className={open ? 'NavDrop-Button active' : 'NavDrop-Button'}
+        onClick={handleClose}
+      >
+        {buttonText}
+      </a>
+      {open ? (
+        <ul className="NavDrop-Menu">
+          <li className="NavDrop-Item">
+            <a
+              onClick={handleClose}
+              href={href1}
+              target={target1}
+              className="NavDrop-Link"
+            >
+              {droptext}
+            </a>
+          </li>
+          <li className="NavDrop-Item">
+            <a
+              onClick={handleClose}
+              href={href2}
+              target={target2}
+              className="NavDrop-Link"
+            >
+              {droptext2}
+            </a>
+          </li>
+          <li className="NavDrop-Item">
+            <a
+              onClick={handleClose}
+              href={href3}
+              target={target3}
+              className="NavDrop-Link"
+            >
+              {droptext3}
+            </a>
+          </li>
+        </ul>
+      ) : null}
+    </div>
+  );
+};
