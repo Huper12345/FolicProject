0d874c9aa54c3651001b016f93162c4a9a6f3831
Huper12345 <115031997+Huper12345@users.noreply.github.com>
2023-02-01 15:39:49 +0300

    Update scrool top button

--- src/components/Footer/FooterNav/FooterNav.js
+++ src/components/Footer/FooterNav/FooterNav.js
@@ -1,68 +1,74 @@
 import React from 'react';
 import './FooterNav.css';
 
-import { Link, animateScroll as scroll } from "react-scroll";
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 export const FooterNav = () => {
   return (
     <nav className="Footer-Nav">
-      <Link 
-      to="Medication"
-      spy={true}
-      smooth={true}
-      offset={-70}
-      duration={600}
-      className="Footer-Nav-Link">
+      <Link
+        to="Medication"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={600}
+        className="Footer-Nav-Link"
+      >
         О препарате
       </Link>
 
-      <Link 
-       to="DoctorQuestion"
-       spy={true}
-       smooth={true}
-       offset={-70}
-       duration={600}
-       className="Footer-Nav-Link">
+      <Link
+        to="DoctorQuestion"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={600}
+        className="Footer-Nav-Link"
+      >
         Задать вопрос
       </Link>
 
-      <Link 
-      to="GoodForYou"
-      spy={true}
-      smooth={true}
-      offset={-70}
-      duration={600}
-      className="Footer-Nav-Link">
+      <Link
+        to="GoodForYou"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={600}
+        className="Footer-Nav-Link"
+      >
         Полезные советы беременным
       </Link>
 
-      <Link 
-      to="BabyCalculator"
-      spy={true}
-      smooth={true}
-      offset={-70}
-      duration={600}
-      className="Footer-Nav-Link">
+      <Link
+        to="BabyCalculator"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={600}
+        className="Footer-Nav-Link"
+      >
         Калькулятор пола ребенка
       </Link>
 
-      <Link 
-      to="GoodStuff"
-      spy={true}
-      smooth={true}
-      offset={-70}
-      duration={600}
-      className="Footer-Nav-Link">
+      <Link
+        to="GoodStuff"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={600}
+        className="Footer-Nav-Link"
+      >
         Полезные материалы для беременных
       </Link>
 
-      <Link 
-      to="FAquestions"
-      spy={true}
-      smooth={true}
-      offset={-70}
-      duration={600}
-      className="Footer-Nav-Link">
+      <Link
+        to="FAquestions"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={600}
+        className="Footer-Nav-Link"
+      >
         Часто задаваемые вопросы
       </Link>
 
@@ -70,7 +76,7 @@ export const FooterNav = () => {
         href="https://www.rceth.by/NDfiles/instr/23_01_3103_s.pdf"
         target="blank"
         className="TextButton Footer"
-      >
+      ></a>
     </nav>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,41 +1,39 @@
-import React from "react"
-import "./Header.scss";
-import "../../pages/Home/Home.scss"
-import { Logo } from "./Logo";
-import { Nav } from "./Nav";
-import { useEffect } from "react";
-import { useState } from "react";
+import React from 'react';
+import './Header.scss';
+import '../../pages/Home/Home.scss';
+import { Logo } from './Logo';
+import { Nav } from './Nav';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export const Header = () => {
-  
-  const [sticky, setSticky] = useState("");
-
+  const [sticky, setSticky] = useState('');
 
   // on render, set listener
   useEffect(() => {
-    window.addEventListener("scroll", isSticky);
+    window.addEventListener('scroll', isSticky);
     return () => {
-      window.removeEventListener("scroll", isSticky);
+      window.removeEventListener('scroll', isSticky);
     };
   }, []);
 
   const isSticky = () => {
-    const introHeight = document.querySelector("#Intro").offsetHeight
+    const introHeight = document.querySelector('#Intro').offsetHeight;
     const scrollTop = window.scrollY;
-    const stickyClass = scrollTop >= introHeight ? "is-sticky" : "";
+    const stickyClass = scrollTop >= introHeight ? 'is-sticky' : '';
     setSticky(stickyClass);
   };
 
   const classes = `Header ${sticky}`;
-  
+
   return (
-    <div  id="Header" className={classes}>
+    <div id="Header" className={classes}>
       <div className="container">
         <header id="Header__inner" className="Header__inner">
-            <Logo />
-            <Nav />
-          </header>
-        </div>
+          <Logo />
+          <Nav />
+        </header>
+      </div>
     </div>
   );
 };
