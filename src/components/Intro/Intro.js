5ac5c6c756cbeb52c1893ad0bc40b56a53f93190
Kirill <huper12345@gmail.com>
2023-02-03 07:48:57 +0000

    Update Images

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import './Intro.scss';
-import Girl from '../../helpers/assets/Images/dev2.webp';
-import { TextBlock } from './TextBlock';
-
+import React from "react";
+import "./Intro.scss";
+import Girl from "../../helpers/assets/Images/GirlOne.webp";
+import { TextBlock } from "./TextBlock";
 
 export const Intro = () => {
-  
-  
   return (
     <div id="Intro" className="Intro">
       <div className="container">
         <div className="Intro-Inner">
           <TextBlock />
-          <img
-            className="Intro-Background"
-            alt="Girl"
-            src="https://i.ibb.co/1MyHGxF/devushka6.png"
-          ></img>
+          <img className="Intro-Background" alt="Girl" src={Girl}></img>
         </div>
       </div>
     </div>
