38fad7b10cbceced21c3998711e34b7e1dea3ba7
Kirill <huper12345@gmail.com>
2023-02-03 10:33:54 +0000

    Update Images for GoodStuff and Footer Logo block's

--- src/components/Footer/LogoBorimed/LogoBorimed.js
+++ src/components/Footer/LogoBorimed/LogoBorimed.js
@@ -1,11 +1,21 @@
-import React from "react"
+import React from "react";
 import "./LogoBorimed.css";
-
+import LogoFooter from "../../../helpers/assets/Images/Logo/logoBorimed.webp";
 
 export const LogoBorimed = () => {
   return (
     <div className="Logo-Borimed">
-      <a target="blank" className="Logo-Borimed-Link" href="https://borimed.com/"><img src="https://i.ibb.co/X4pTzKt/logo-Borimed.png" alt="Logo-Borimed" className="Logo-Borimed-Img"></img></a>
+      <a
+        target="blank"
+        className="Logo-Borimed-Link"
+        href="https://borimed.com/"
+      >
+        <img
+          src={LogoFooter}
+          alt="Logo-Borimed"
+          className="Logo-Borimed-Img"
+        ></img>
+      </a>
     </div>
   );
 };
