47cd01d65b8a50f5798c259ef279fe9a54ed0b4f
Huper12345 <huper12345@gmail.com>
2023-02-05 21:00:21 +0300

    add girl lazyload

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Intro.scss";
 import Girl from "../../helpers/assets/Images/GirlOne.webp";
+import GirlP from "../../helpers/assets/Images/GirlPlaceholder/GirlOne.webp";
 import { TextBlock } from "./TextBlock";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export const Intro = () => {
   return (
@@ -9,7 +11,13 @@ export const Intro = () => {
       <div className="container">
         <div className="Intro-Inner">
           <TextBlock />
-          <img className="Intro-Background" alt="Girl" src={Girl}></img>
+          <LazyLoadImage 
+          className="Intro-Background" 
+          alt="Girl" 
+          src={Girl}
+          placeholderSrc={GirlP}
+          effect="blur"
+          />
         </div>
       </div>
     </div>
