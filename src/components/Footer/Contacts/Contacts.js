3880164476b34c093644d3977f31fecba272777e
Kirill <huper12345@gmail.com>
2023-03-02 07:42:02 +0000

    Add company adress to micro markup

--- src/components/Footer/Contacts/Contacts.js
+++ src/components/Footer/Contacts/Contacts.js
@@ -3,12 +3,25 @@ import "./Contacts.css";
 import { TextButton } from "../../../helpers/TextButton";
 
 export const Contacts = () => {
+  const articleStructuredData = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "открытое акционерное общество «Борисовский завод медицинских препаратов»",
+    address: "Республика Беларусь, г. Борисов, ул. Чапаева, 64",
+    telephone: "+375(177) 76 71 96",
+    zipcode: "222518",
+  };
+
   return (
     <div className="Contacts-Container" id="Contacts">
+      <script type="application/ld+json">
+        {JSON.stringify(articleStructuredData)}
+      </script>
+
       <h2 className="Contacts-Title">
-        Производитель - открытое акционерное общество «Борисовский завод
-        медицинских препаратов» Республика Беларусь, г. Борисов, ул. Чапаева,
-        64, 222518, +375(177) 76 71 96
+        Производитель - {articleStructuredData.name} ,
+        {articleStructuredData.address}, {articleStructuredData.zipcode},{" "}
+        {articleStructuredData.telephone}
       </h2>
       <h3 className="Contacts-Subtext">
         ЛЕКАРСТВЕННЫЙ ПРЕПАРАТ. ПЕРЕД ПРИМЕНЕНИЕМ ОЗНАКОМЬТЕСЬ С ИНСТРУКЦИЕЙ И
